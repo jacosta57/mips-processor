17161bc558b9be51e1831aef2b7347951bcbfc65
NakotaC <150291473+NakotaC@users.noreply.github.com>
2024-10-27 21:34:37 -0500

    Fixing issues for synthesis. Muxes didn't want to work

--- cpre381-toolflow/proj/src/Muxes/mux2t1_N.vhd
+++ cpre381-toolflow/proj/src/Muxes/mux2t1_N.vhd
@@ -21,24 +21,11 @@ entity mux2t1_N is
 end mux2t1_N;
 
 
-architecture structural of mux2t1_N is
+architecture Behavioral of mux2t1_N is
+	begin
+	o_O <= i_D0 when (i_S = '0') else
+		i_D1 when (i_S = '1') else
+		x"00000000";
 
-  component mux2t1 is
-    port(i_S                  : in std_logic;
-         i_D0                 : in std_logic;
-         i_D1                 : in std_logic;
-         o_O                  : out std_logic);
-  end component;
-
-begin
-
-  -- Instantiate N mux instances.
-  G_NBit_MUX: for i in 0 to N-1 generate
-    MUXI: mux2t1 port map(
-              i_S      => i_S,      -- All instances share the same select input.
-              i_D0     => i_D0(i),  -- ith instance's data 0 input hooked up to ith data 0 input.
-              i_D1     => i_D1(i),  -- ith instance's data 1 input hooked up to ith data 1 input.
-              o_O      => o_O(i));  -- ith instance's data output hooked up to ith data output.
-  end generate G_NBit_MUX;
   
-end structural;
+	end Behavioral;
