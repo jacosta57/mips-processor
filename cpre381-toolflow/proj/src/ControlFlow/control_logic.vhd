f79c475d8810ffac7a7ebc241cdb5e44741a17af
NakotaC <150291473+NakotaC@users.noreply.github.com>
2024-11-21 14:55:57 -0600

    Fixed sll being incorrect. Jal is only issue with basetest.

--- cpre381-toolflow/proj/src/ControlFlow/control_logic.vhd
+++ cpre381-toolflow/proj/src/ControlFlow/control_logic.vhd
@@ -67,10 +67,6 @@ begin
                 r_control.ALUOp    <= "10";
                 r_control.RegWrite <= '1';
                 
-                if to_integer(unsigned(i_funct)) = 0
-                then
-        r_control <= ('0', '0', '0', '0', '0', '0', '0', '0', "00", '0', '0', '0', '0');
-                end if;
 
                 -- Special case for jr
                 if to_integer(unsigned(i_funct)) = 8 then
